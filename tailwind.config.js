3b38a5043e855581c297f869a44beaf3f1bed9e1
awaisnazar001 <awaisnazarofficial@gmail.com>
2022-08-05 16:17:02 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    screens: {
+
+    },
+    extend: {
+      colors: {
+        lightBlue: '#F5F8FA',
+        grayBluish: '#F9FAF5',
+        skyBlue: '#1FB5EB',
+        lightSkyBlue: '#C7DFEF',
+        darkBlue: '#4664C7',
+        darkGray: "#333333",
+        brightRed: "#FF5F5F",
+        lightRed: '#FE9699',
+        shadowColor: '#FF5F5F',
+        lightYellow: '#FED86F'
+      }
+    },
+  },
+  plugins: [],
+}
